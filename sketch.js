c8a40809ee692bab79bf8aef853fabf0d4fd4c7e
hanjoes <hanzhou87@gmail.com>
2016-03-22 14:27:26 -0700

    Heart is now ponding.

--- sketch.js
+++ sketch.js
@@ -6,13 +6,17 @@ var ph;
 var cw = 200;
 var ch = 200;
 
+var PONDING_SPEED = 0.3;
+var WIDTH = 7;
+
 function setup() {
   createCanvas(cw, ch);
-  // 9 seems the best number.
-  ph = new PondingHeart(9, cw, ch);
+  // 7 seems the best number.
+  ph = new PondingHeart(WIDTH, cw, ch);
 }
 
 function draw() {
+  background("white");
   ph.startPonding();
 }
 
@@ -24,24 +28,26 @@ var HeartPiece = function(cx, cy, ox, oy) {
   this.oy = oy;
   this.width = 10;
   this.height = 10;
-  this.fillColor = 'red';
+  this.fillColor = color(200, 0, 0);
 };
 
-HeartPiece.prototype.show = function() {
+HeartPiece.prototype.show = function(t) {
   rectMode(CENTER);
   fill(this.fillColor);
-  print("ox: " + this.ox + " oy: " + this.oy);
-  rect(this.cx, this.cy, this.width, this.height);
+  noStroke();
+  rect(this.cx + this.ox * t, this.cy + this.oy * t, this.width, this.height);
 };
 
 var PondingHeart = function(w, cw, ch) {
   this.width = w;
   this.relaxDist = 1;
-  this.tenseDist = 5;
+  this.tenseDist = 6;
   this.pieces = [];
   this.centerX = cw / 2;
   this.centerY = ch / 2;
   this.r = 5;
+  this.speed = PONDING_SPEED;
+  this.currentTranslation = 0;
   // initialize the bottom half include the middle lane
   // include zero since the bottom lane contains only
   // the center piece
@@ -72,7 +78,7 @@ PondingHeart.prototype.initLane = function(level, hasMiddlePiece) {
 PondingHeart.prototype.initHalf = function(step, level) {
   for (var i = 1; i <= (this.width - 2 * Math.abs(level)) / 2; ++i) {
     this.pieces.push(new HeartPiece(
-      this.centerX + i * step * (this.relaxDist + 2 * this.r), 
+      this.centerX + i * step * (this.relaxDist + 2 * this.r),
       this.centerY + level * (this.relaxDist + 2 * this.r),
       i * step,
       level));
@@ -80,7 +86,17 @@ PondingHeart.prototype.initHalf = function(step, level) {
 };
 
 PondingHeart.prototype.startPonding = function() {
+  var t = this.currentTranslation;
+  var speed = this.speed;
+  // show translated pieces
   this.pieces.forEach(function(h) {
-    h.show();
+    h.show(t);
   });
+  // update states
+  t += speed;
+  if (t > this.tenseDist || t < 0) {
+    speed = -speed;
+  }
+  this.speed = speed;
+  this.currentTranslation = t;
 }
